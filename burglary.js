6639264ce7c6fc8d5f164537f69fe215d4599217
Onur Uyar <me@onuruyar.com>
2015-09-25 13:14:43 +0100

    heatmap and marker cluster added

--- burglary.js
+++ burglary.js
@@ -142,10 +142,10 @@ if (Meteor.isServer) {
     //TODO: get country code by location
     countryCode = 'GB' 
     // today
-    startDate = new Date()
-    // CrimesFDS.TIMESPAN_IN_MONTHS back from today
     endDate = new Date()
-    endDate.setMonth(endDate.getMonth() - CrimesFDS.TIMESPAN_IN_MONTHS)
+    // CrimesFDS.TIMESPAN_IN_MONTHS back from today
+    startDate = new Date()
+    startDate.setMonth(startDate.getMonth() - CrimesFDS.TIMESPAN_IN_MONTHS)
     // find cached data from local db
     crimes = Crimes.find({
       'location': {
@@ -154,7 +154,7 @@ if (Meteor.isServer) {
               type : "Point",
               coordinates : [params.lat, params.lng]
            },
-           $maxDistance: 1600
+           $maxDistance: MAX_DISTANCE
         }
       },
       date: {$gt: startDate, $lte: endDate}
@@ -197,241 +197,92 @@ if (Meteor.isServer) {
 }
 
 if (Meteor.isClient) {
-  // Meteor.startup(function() {
-  //   GoogleMaps.load()
-  // })
+  const MAP_ZOOM = 15
+  const HEATMAP_OPACITY = 0.2
+  const HEATMAP_RADIUS = 75
+  const ACTIVE_LOCATION = 'activelocation'
+
+  let markers = {}, infoWindow = null, heatmap = null, markerCluster = null
+
+  Meteor.startup(() => {
+    GoogleMaps.load({ v: '3', libraries: 'visualization' })
+    GoogleMaps.loadUtilityLibrary('http://google-maps-utility-library-v3.googlecode.com/svn/tags/markerclustererplus/2.1.2/markerclustererplus/src/markerclusterer_packed.js')
+  })
+
   // Map
-  // Template.map.helpers({
-  //   geolocationError: function() {
-  //     var error = Geolocation.error()
-  //     return error && error.message
-  //   },
-  //   mapOptions: function() {
-  //     var latLng = Geolocation.latLng()
-  //     // Initialize the map once we have the latLng.
-  //     if (GoogleMaps.loaded() && latLng) {
-  //       return {
-  //         center: new google.maps.LatLng(latLng.lat, latLng.lng),
-  //         zoom: MAP_ZOOM
-  //       }
-  //     }
-  //   }
-  // })
+  Template.map.helpers({
+    geolocationError: function() {
+      var error = Geolocation.error()
+      return error && error.message
+    },
+    mapOptions: function() {
+      // set active location from geolocation from device 
+      var latLng = Geolocation.latLng()
+      Session.set(ACTIVE_LOCATION, latLng)
+      // Initialize the map once we have the latLng.
+      if (GoogleMaps.loaded() && latLng) {
+        return {
+          center: new google.maps.LatLng(latLng.lat, latLng.lng),
+          zoom: MAP_ZOOM
+        }
+      }
+    }
+  })
   Template.map.onCreated(function () {
-    this.autorun(function () {
-      // subscribe to crimes for current location
-      var loc = Geolocation.latLng()
-      if (loc) Meteor.subscribe("crimes", {lat: loc.lat, lng: loc.lng})
+    // maps ready?
+    GoogleMaps.ready('map', (map) => {
+      infoWindow = new google.maps.InfoWindow()
+      this.autorun(function () {
+        // subscribe to crimes for current location
+        let loc = Session.get(ACTIVE_LOCATION), crimes = Crimes.find(), heatmapData, markerClusterData
+        if (loc) Meteor.subscribe("crimes", {lat: loc.lat, lng: loc.lng})
+
+        crimes.forEach((crime) => {
+          let position = new google.maps.LatLng(...crime.location.coordinates)
+          if (markers[crime._id]) {
+              markers[crime._id].setPosition(position)
+          }else{
+            markers[crime._id] = new google.maps.Marker({
+              position: position,
+              map: map.instance
+            })
+            markers[crime._id].addListener('click', function() {
+              map.instance.setCenter(position)
+              infoWindow.setContent(`<pre>${JSON.stringify(crime, null, 2)}</pre>`)
+              infoWindow.open(map.instance, markers[crime._id])
+              // if (crime.owner === Meteor.userId()) {
+              //   Session.set(ACTIVE_INCIDENT, incident._id)
+              // }
+            });
+          }
+        })
+
+        heatmapData = crimes.map((crime) => new google.maps.LatLng(...crime.location.coordinates))
+        if (heatmap) {
+          heatmap.setData(heatmapData)
+        } else {
+          heatmap = new google.maps.visualization.HeatmapLayer({
+            data: heatmapData,
+            map: map.instance,
+            radius: HEATMAP_RADIUS,
+            opacity: HEATMAP_OPACITY
+          })
+        }
+
+        markerClusterData = _.values(markers)
+        if (markerCluster) {
+          markerCluster.clearMarkers()
+          markerCluster.addMarkers(markerClusterData)
+        }else{
+          markerCluster = new MarkerClusterer(map.instance, markerClusterData)
+        }
+
+      })
+      // when user drag maps, change active location to map center
+      map.instance.addListener('center_changed', function() {
+        let mapCenter = map.instance.getCenter()
+        Session.set(ACTIVE_LOCATION, {lat: mapCenter.G, lng: mapCenter.K})
+      })
     })
   })
 }
-
-
-// Incidents = new Mongo.Collection("incidents")
-//
-// if (Meteor.isClient) {
-//   var MAP_ZOOM = 15
-//   var SEARCH_RADIUS = 25
-//   var ACTIVE_INCIDENT = 'activeIncident'
-//   var INCIDENT_UPDATED = 'incidentUpdated'
-//   var LOCATION = 'location'
-//   var markers = {}
-//
-//   Meteor.startup(function() {
-//     GoogleMaps.load()
-//   })
-//
-//   // reset session
-//   Session.setDefault(ACTIVE_INCIDENT, null)
-//   Session.setDefault(INCIDENT_UPDATED, false)
-//
-//   // subscribe user incidents
-//   Meteor.subscribe("incidents")
-//
-//   // Incidents
-//   Template.incidentForm.helpers({
-//     hasActiveIncident: function () {
-//       return Session.get(ACTIVE_INCIDENT)
-//     },
-//     activeIncident: function () {
-//       return Incidents.findOne(Session.get(ACTIVE_INCIDENT))
-//     }
-//   })
-//   Template.incidentForm.events({
-//     'submit form': function (e) {
-//       var incidentId, data
-//
-//       e.preventDefault()
-//       // if session has active incident created previously call update
-//       incidentId = Session.get(ACTIVE_INCIDENT)
-//       if (incidentId) {
-//         data = {
-//           brand: e.target.brand.value,
-//           model: e.target.model.value,
-//           price: e.target.price.value
-//         }
-//         Meteor.call("updateIncident", incidentId, data, function (error, result) {
-//           if (error) {
-//             alert('Ops!, something went wrong', error)
-//           } else {
-//             Session.setDefault(INCIDENT_UPDATED, true)
-//           }
-//         })
-//       // if a new incident add it
-//       } else {
-//         data = Geolocation.latLng()
-//         if (!data) return alert('Location not found, please enable geolocation')
-//         Meteor.call("addIncident", data, function (error, result) {
-//           if (error) {
-//             alert('Ops!, something went wrong', error)
-//           } else {
-//             Session.set(ACTIVE_INCIDENT, result)
-//           }
-//         })
-//       }
-//     },
-//     'reset form': function () {
-//       var incidentId = Session.get(ACTIVE_INCIDENT)
-//       if (incidentId) {
-//         Meteor.call('removeIncident', incidentId, function (error, result) {
-//           if (error) {
-//             alert('Ops!, something went wrong', error)
-//           } else {
-//             Session.set(INCIDENT_UPDATED, false)
-//             Session.set(ACTIVE_INCIDENT, null)
-//             if (markers[incidentId]) markers[incidentId].setMap(null)
-//           }
-//         })
-//       }
-//     }
-//   })
-//
-//   // Map
-//   Template.map.helpers({
-//     geolocationError: function() {
-//       var error = Geolocation.error()
-//       return error && error.message
-//     },
-//     mapOptions: function() {
-//       var latLng = Geolocation.latLng()
-//       // Initialize the map once we have the latLng.
-//       if (GoogleMaps.loaded() && latLng) {
-//         return {
-//           center: new google.maps.LatLng(latLng.lat, latLng.lng),
-//           zoom: MAP_ZOOM
-//         }
-//       }
-//     }
-//   })
-//
-//   Template.map.onCreated(function() {
-//     var self = this
-//
-//     GoogleMaps.ready('map', function(map) {
-//       // var marker
-//       // var markers = {}
-//
-//       // Create and move the marker when latLng changes.
-//       self.autorun(function() {
-//         var incidents
-//         var latLng = Geolocation.latLng()
-//         if (! latLng) return
-//
-//         // If the marker doesn't yet exist, create it.
-//         // if (! marker) {
-//         //   marker = new google.maps.Marker({
-//         //     position: new google.maps.LatLng(latLng.lat, latLng.lng),
-//         //     map: map.instance
-//         //   })
-//         // }
-//         // // The marker already exists, so we'll just change its position.
-//         // else {
-//         //   marker.setPosition(latLng)
-//         // }
-//
-//         // Center and zoom the map view onto the current position.
-//         // map.instance.setCenter(marker.getPosition())
-//         map.instance.setCenter(latLng)
-//         map.instance.setZoom(MAP_ZOOM)
-//
-//         // find incidents around location
-//         incidents = Incidents.find({
-//           'location.latlng': {
-//             $near: [latLng.lat, latLng.lng], $maxDistance: SEARCH_RADIUS
-//           }
-//         })
-//
-//         incidents.forEach(function (incident) {
-//           if (markers[incident._id]) {
-//             markers[incident._id].setPosition(incident.location.latlng)
-//           }else{
-//             markers[incident._id] = new google.maps.Marker({
-//               position: incident.location.latlng,
-//               map: map.instance,
-//               title: incident._id
-//             })
-//             markers[incident._id].addListener('click', function() {
-//               map.instance.setCenter(markers[incident._id].getPosition())
-//               if (incident.owner === Meteor.userId()) {
-//                 Session.set(ACTIVE_INCIDENT, incident._id)
-//               }
-//             });
-//           }
-//         })
-//       })
-//     })
-//   })
-// }
-//
-// if (Meteor.isServer) {
-//   // Methods
-//   Meteor.methods({
-//     addIncident: function (latLng) {
-//       // Make sure the user is logged in before inserting a task
-//       if (! Meteor.userId()) {
-//         throw new Meteor.Error("not-authorized")
-//       }
-//
-//       // meteor doesn't dupport mongodb native geospatial search for now
-//       // so save location for both new and old version
-//       return Incidents.insert({
-//         location: {
-//           // this is for future
-//           // more info and examples: http://tugdualgrall.blogspot.co.uk/2014/08/introduction-to-mongodb-geospatial.html
-//           geo: {
-//             type: 'Point',
-//             coordinates: [latLng.lat, latLng.lng]
-//           },
-//           // this is currently in use
-//           latlng: latLng
-//         },
-//         createdAt: new Date(),
-//         owner: Meteor.userId()
-//       })
-//     },
-//     updateIncident: function (incidentId, data) {
-//       var incident = Incidents.findOne(incidentId)
-//       // Make sure only the incident owner can update the incident
-//       if (incident.owner !== Meteor.userId()) {
-//         throw new Meteor.Error("not-authorized")
-//       }
-//
-//       return Incidents.update(incidentId, {$set :{details: data}})
-//     },
-//     removeIncident: function (incidentId) {
-//       var incident = Incidents.findOne(incidentId)
-//       // Make sure only the incident owner can update the incident
-//       if (incident.owner !== Meteor.userId()) {
-//         throw new Meteor.Error("not-authorized")
-//       }
-//
-//       return Incidents.remove(incidentId)
-//     }
-//   })
-//
-//   // publish user incidents to client
-//   Meteor.publish("incidents", function () {
-//     return Incidents.find({owner: this.userId})
-//   })
-//
-// }
